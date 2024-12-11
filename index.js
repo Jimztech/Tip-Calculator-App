ed327399ed3f4dad27cd3a6b72e4d6cf1017fc82
jimztech <ibezimjames20@gmail.com>
2024-12-11 14:56:39 +0100

    working on js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-//const billValue = document.getElementById("bill");
-//const numPeopleValue = document.getElementById("numbers");
+const billValue = document.getElementById("bill");
+const numPeopleValue = document.getElementById("number");
+const tipPercentage = document.getElementsByClassName("tip-button");
+const customInput = document.getElementById("custom");
+const amountOutput = document.getElementsByClassName("amount-output");
+const totalOutput = document.getElementsByClassName("total-output");
 
-document.addEventListener('keydown', event => {
-    console.log(`A key is pressed: ${event.key}`)
-});
-
-document.addEventListener('keyup', event => {
-    console.log(`A key is released: ${event.key}`)
-})
+function calculateTipPerPerson(billValue, numPeopleValue, tipPercentage) {
+    const errorMessage = document.querySelector(".error-message");
+}
